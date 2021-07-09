f52d52436303427d2098e140b8f67d4ef9012085
Gregory Ashton <gregory.ashton@ligo.org>
2021-07-09 11:38:34 +0100

    Switch to versioneer

--- setup.py
+++ setup.py
@@ -1,45 +1,9 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-import subprocess
 import os
 
-
-def write_version_file(version):
-    """ Writes a file with version information to be used at run time
-
-    Parameters
-    ----------
-    version: str
-        A string containing the current version information
-
-    Returns
-    -------
-    version_file: str
-        A path to the version file
-
-    """
-    try:
-        git_log = subprocess.check_output(
-            ['git', 'log', '-1', '--pretty=%h %ai']).decode('utf-8')
-        git_diff = (subprocess.check_output(['git', 'diff', '.']) +
-                    subprocess.check_output(
-                        ['git', 'diff', '--cached', '.'])).decode('utf-8')
-        if git_diff == '':
-            git_status = '(CLEAN) ' + git_log
-        else:
-            git_status = '(UNCLEAN) ' + git_log
-    except Exception as e:
-        print("Unable to obtain git version information, exception: {}"
-              .format(e))
-        git_status = ''
-
-    version_file = '.version'
-    if os.path.isfile(version_file) is False:
-        with open('src/' + version_file, 'w+') as f:
-            f.write('{}: {}'.format(version, git_status))
-
-    return version_file
+import versioneer
 
 
 def get_long_description():
@@ -50,15 +14,6 @@ def get_long_description():
     return long_description
 
 
-# get version info from __init__.py
-def readfile(filename):
-    with open(filename) as fp:
-        filecontents = fp.read()
-    return filecontents
-
-
-VERSION = '0.0.2'
-version_file = write_version_file(VERSION)
 long_description = get_long_description()
 
 setup(name='koala_html',
@@ -68,10 +23,11 @@ setup(name='koala_html',
       author='Greg Ashton',
       author_email='gregory.ashton@ligo.org',
       license="MIT",
-      version=VERSION,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       packages=['koala_html'],
       package_dir={'koala_html': 'src'},
-      package_data={'koala_html': [version_file, "simple_style.css"]},
+      package_data={'koala_html': ["simple_style.css"]},
       entry_points={'console_scripts':
                     ['koala_table=koala_html.table:main']
                     },
